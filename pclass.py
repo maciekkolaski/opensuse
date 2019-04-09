a751d09f41883b6187bd0c3688eeb61e92183eb0
maciekkolaski <maciej.kolaski@us.edu.pl>
2019-04-09 14:48:27 +0200

    wieksze klasy

--- pclass.py
+++ pclass.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+class MyClass:
+    def __init__(self, name=None, lastname=None):
+        self.name = name
+        self.lastname = lastname
+
+class KolaskiFamily(MyClass):
+    def __init__(self, name):
+        super().__init__(name, "Kolaski")
+
+class FullData(MyClass):
+    def __init__(self, name, lastname, age):
+        super().__init__(name, lastname)
+        self.age = age
